bf6e6efc14512d9f3db86f4d28b4f7ab4cd7870b
BaptisteCoulon <baptistecoulon@yahoo.fr>
2022-01-21 13:42:04 +0100

    Ajoue ASceance en mvc with out js

--- src/main/java/gestionCinema/ihm/GestionCinemaController.java
+++ src/main/java/gestionCinema/ihm/GestionCinemaController.java
@@ -1,14 +1,20 @@
 package gestionCinema.ihm;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import gestionCinema.bll.FilmManager;
+import gestionCinema.bll.SalleManager;
 import gestionCinema.bll.SceanceManager;
 import gestionCinema.bo.Film;
+import gestionCinema.bo.Sceance;
 
 @Controller
 public class GestionCinemaController {
@@ -17,6 +23,8 @@ public class GestionCinemaController {
 	SceanceManager manager;
 	@Autowired
 	FilmManager managerfilm;
+	@Autowired 
+	SalleManager managersalle;
 	@GetMapping("/index")
 	public String index(Model model) {
 		model.addAttribute("lstSceance",manager.selectAll());
@@ -30,6 +38,21 @@ public class GestionCinemaController {
 		model.addAttribute("film",film);
 		return "show";
 	}
-	
-	
+	@GetMapping("/ASceance")
+	public String ASceance(Model model) {
+		model.addAttribute("listDeFilm",managerfilm.selectAll());
+		model.addAttribute("lstSalle",managersalle.selectAll());
+		model.addAttribute("sceance",new Sceance());
+		model.addAttribute("lstSceance",manager.selectAll());
+		return "ASceance";
+		
+	}
+
+	@PostMapping("/add")
+	public String add(@ModelAttribute("sceance")Sceance sceance) {
+		
+		manager.insert(sceance);
+		
+		return "redirect:ASceance";
+	}
 }
