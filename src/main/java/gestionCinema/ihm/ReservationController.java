6bf0d43a3f5d26cf4b2d3c06ee435bbce43f9ba5
Gwendal LEPLANT <gwendal.leplant@live.fr>
2022-01-21 15:50:32 +0100

    add ReservationController to add reservation feature through SpringMVC

--- src/main/java/gestionCinema/ihm/ReservationController.java
+++ src/main/java/gestionCinema/ihm/ReservationController.java
@@ -3,6 +3,7 @@ package gestionCinema.ihm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,29 +18,29 @@ import gestionCinema.bo.Reservation;
 @Controller
 @RequestMapping("/reservation")
 public class ReservationController {
-	
+
 	@Autowired
 	ClientManager clientManager;
-	
+
 	@Autowired
 	SceanceManager sceanceManager;
-	
+
 	@Autowired
 	ReservationManager reservationManager;
-	
+
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("lstSceance",sceanceManager.selectAll());
+		model.addAttribute("lstSceance", sceanceManager.selectAll());
 		model.addAttribute("lstClient", clientManager.selectAll());
 		model.addAttribute("reservation", new Reservation());
 		return "reservation";
-		
+
 	}
-	
+
 	@PostMapping("/add")
-	public String add(@ModelAttribute("reservation")Reservation reservation) throws ReservationException {		
-		reservationManager.insert(reservation);		
-		return "redirect:reservation";
+	public String add(Model model, Reservation reservation) throws ReservationException {
+		reservationManager.insert(reservation);
+		return "redirect:/reservation";
 	}
 
 }
