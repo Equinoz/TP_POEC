85c8e63dbdb7139d8deffafc3c307a408053212a
Gwendal LEPLANT <gwendal.leplant@live.fr>
2022-01-26 14:44:51 +0100

    update show feature

--- src/main/resources/static/js/show.js
+++ src/main/resources/static/js/show.js
@@ -1,1 +1,11 @@
-console.log("coucou");
+async function loadEnv() {
+    const response = await fetch("../js/env.json");
+    const jsonData = await response.json();
+    apikey = jsonData.apikey;
+}
+
+
+function updateDetails() {
+    document.getElementById('duree').innerHTML = document.getElementById('duree').innerHTML.replace(/PT/i, '').toLowerCase();
+    const apikey = await loadEnv();
+}
